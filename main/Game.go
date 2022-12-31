7d381c2ae7d9b7bbea90f49de763b9d210802a8b
Pablo Pérez García <politrons@gmail.com>
2022-12-31 14:06:56 +0100

    add canvas/input

--- main/Game.go
+++ main/Game.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
@@ -33,13 +34,40 @@ func loadImage(filePath string) image.Image {
 	return imgData.(image.Image)
 }
 
+//Index of background image
+var imageIndex = 0
+
+//Collection of image of the whole game
+var images = []string{
+	"./assets/background.png",
+	"./assets/background2.png",
+}
+
+//Collection of texts of the whole game
+var gameTexts = []string{
+	"aaaaaaaaaaaaaaaa",
+	"bbbbbbbbbbbbbbbb",
+}
+
+//Collection of actions of the whole game
+var imagesActions = [][]string{
+	{"attack"},
+	{"fly"},
+}
+
+type Actions struct {
+	values []string
+}
+
+type ArrayFeature interface {
+	contains(element string) bool
+}
+
 func main() {
 	myApp := app.New()
 	window := myApp.NewWindow("Game")
 
-	background := loadImage("./assets/background.png")
-	background2 := loadImage("./assets/background2.png")
-
+	background := loadImage(images[imageIndex])
 	backgroundImg := canvas.NewImageFromImage(background)
 	backgroundImg.FillMode = canvas.ImageFillOriginal
 
@@ -47,7 +75,7 @@ func main() {
 
 	playerImg := canvas.NewRasterFromImage(sprite)
 
-	addLabel(sprite, 300, 300, "hello world bla bla bla blA,bla,")
+	addLabel(sprite, 300, 300, gameTexts[imageIndex])
 
 	c := container.New(layout.NewMaxLayout(), backgroundImg, playerImg)
 
@@ -65,18 +93,22 @@ func main() {
 			// Holds the string that scanned
 			text := scanner.Text()
 			if len(text) != 0 {
+				actions := Actions{values: imagesActions[imageIndex]}
+				if actions.contains(text) {
+					imageIndex = imageIndex + 1
+					background := loadImage(images[imageIndex])
 
-				if text == "attack" {
-					backgroundImg = canvas.NewImageFromImage(background2)
+					backgroundImg = canvas.NewImageFromImage(background)
 					backgroundImg.FillMode = canvas.ImageFillOriginal
 
-					sprite = image.NewRGBA(background.Bounds())
-					addLabel(sprite, 300, 300, "second text bla bla bla bla bla")
+					sprite := image.NewRGBA(background.Bounds())
+					addLabel(sprite, 300, 300, gameTexts[imageIndex])
 
-					playerImg = canvas.NewRasterFromImage(sprite)
-
-					c = container.New(layout.NewMaxLayout(), backgroundImg, playerImg)
-					window.SetContent(c)
+					appendImage(sprite, backgroundImg, window)
+				} else {
+					sprite := image.NewRGBA(background.Bounds())
+					addLabel(sprite, 400, 400, "Action not valid.")
+					appendImage(sprite, backgroundImg, window)
 				}
 
 				fmt.Println(text)
@@ -94,6 +126,28 @@ func main() {
 	window.CenterOnScreen()
 	window.ShowAndRun()
 
+}
+
+/**
+Function to append the image into the container once we we create a [Raster] instance from
+the original image.
+*/
+func appendImage(sprite *image.RGBA, backgroundImg *canvas.Image, window fyne.Window) {
+	playerImg := canvas.NewRasterFromImage(sprite)
+	c := container.New(layout.NewMaxLayout(), backgroundImg, playerImg)
+	window.SetContent(c)
+}
+
+/**
+Util function to check if an action is part of the array passed
+*/
+func (actions Actions) contains(element string) bool {
+	for _, action := range actions.values {
+		if action == element {
+			return true
+		}
+	}
+	return false
 }
 
 /**
